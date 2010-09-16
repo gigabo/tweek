064fdf35cf34e27e6f6dad854990d8ddcd90b72c
Bo Borgerson <gigabo@gmail.com>
2010-09-16 12:59:29 -0700

    Make a default (coming soon) page

--- tweek.rb
+++ tweek.rb
@@ -5,20 +5,16 @@ require 'punchcard'
 require 'haml'
 require 'less'
 
-get '/' do
-  "My Playground."
-end
-
 get '/css/punch.css' do
   content_type 'text/css', :charset => 'utf-8'
   less :punch
 end
 
-get '/punch' do
-  "Coming soon."
-end
-
 get '/punch/:handle' do
   @chart_url = PunchCard.new(TweetDates.new(params[:handle])).url
   haml :punchcard
+end
+
+get '/*' do
+  haml :coming_soon
 end
