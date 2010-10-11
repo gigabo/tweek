07fe72fbf32d03422bedc7a6f01bcc768369777e
Bo Borgerson <gigabo@gmail.com>
2010-10-11 09:53:42 -0700

    Adjust wording in unit circle description

--- about/unit_circle.rb
+++ about/unit_circle.rb
@@ -7,7 +7,8 @@ def about_this_toy
       {
         :p => <<-EO_P
           Canvas coordinates start at the upper-left corner, and extend down
-          and to the right.  Pixels are the units.  Sometimes this bothers me.
+          and to the right, with pixels as the units.  Sometimes this bothers
+          me.
         EO_P
       },
       {
@@ -15,7 +16,7 @@ def about_this_toy
           I like thinking of positive Y as up.  I don't always want to work
           with pixels as my units of measurement.  Also, I like having the
           origin at the center.  Fortunately the canvas provides a way for me
-          to set all of this up the way I like it: The
+          to set all of this up the way I like it in one easy step: The
           <code>setTransform</code> method.
         EO_P
       },
@@ -23,15 +24,15 @@ def about_this_toy
         :p => <<-EO_P
           Drawing this unit circle is a good example of when it's handy to
           have logical dimensions decoupled from the actual pixel dimensions
-          of the canvas.  Once the scale is set up I can really just draw my
-          circle with a radius of one.
+          of the canvas.  Once the transformation matrix is set up I can
+          really just draw my circle at the origin with a radius of one.  My
+          line segment goes from (0,0) to (cos(theta), sin(theta)).
         EO_P
       },
       {
         :p => <<-EO_P
           This is my current canvas initialization method.  For the unit
-          circle, I just call <code>this.init_canvas(5.0)</code>, and presto:
-          everything's just how I like it.
+          circle, I just call <code>this.init_canvas(5.0)</code>.
           <aside>
           (Not quite sure what's up with the syntax coloring in this gist... I
           guess CoffeeScript support isn't quite there yet)
