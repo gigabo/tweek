dfadb061bd55da234ff18cd74e9bea4b6d66a11f
Bo Borgerson <gigabo@gmail.com>
2010-10-19 16:41:08 -0700

    Be a little bit stricter on linking

--- coffee/link_deps.coffee
+++ coffee/link_deps.coffee
@@ -2,7 +2,7 @@
 $(document).ready () =>
   $(".highlight").find(".s1").each (i, e) =>
     text = $(e).text()
-    if text.match(/^'\w(\w|\/)+'$/)
+    if text.match(/^'\w+\/(\w|\/)+'$/)
       text = text.replace(/^'|'$/g,'')
       newtext = "'<a href='/docs/coffee/#{text}.html'>#{text}</a>'"
       $(e).html(newtext)
--- public/js/link_deps.js
+++ public/js/link_deps.js
@@ -6,7 +6,7 @@
     return $(".highlight").find(".s1").each(__bind(function(i, e) {
       var newtext, text;
       text = $(e).text();
-      if (text.match(/^'\w(\w|\/)+'$/)) {
+      if (text.match(/^'\w+\/(\w|\/)+'$/)) {
         text = text.replace(/^'|'$/g, '');
         newtext = ("'<a href='/docs/coffee/" + (text) + ".html'>" + (text) + "</a>'");
         return $(e).html(newtext);
